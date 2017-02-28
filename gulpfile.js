21f419ebffc5bb8a26d44ec5bfb1bd6efe371f8d
Jesse Kramer <kram0021@hz.nl>
2017-02-28 20:47:52 +0100

    addded build script for sass

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+
+var config = {
+    scssPath: './scss/**/*.scss',
+    cssOutput: './dist/css',
+};
+
+gulp.task('sass-dev', function () {
+    return gulp.src(config.scssPath)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(config.cssOutput));
+});
+gulp.task('sass-prod', function () {
+    return gulp.src(config.scssPath)
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(config.cssOutput));
+});
+gulp.task('watch', function () {
+    gulp.watch(config.scssPath, ['sass-dev', 'sass-prod']);
+});
